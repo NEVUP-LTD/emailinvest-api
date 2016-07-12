0e3718df0558acb968c100f23f9202efa2232c71
Ivelin <Ivelin@Ifko>
2016-07-12 16:12:31 +0300

    Create new API

--- Examples/groupsedit.php
+++ Examples/groupsedit.php
@@ -1,15 +1,22 @@
 <?php
-include  'LatestApi/emailinvest.php';
+
+/*
+ * Copyright (C) 2016 Emailinvest API.
+ * 
+ * Edit your group
+ */
+
+include 'LatestApi/emailinvest.php';
 
 use api;
-$ei=new api\emailinvest("your_api_key", "your_username", true);
-$ei->screen="array"; // - option for php array
-$ei->host="app1.emailinvest.com/api";	
+
+$ei = new api\emailinvest("your_api_key", "your_username", true);
+$ei->screen = "array"; // - option for php array
+$ei->host = "app1.emailinvest.com/api";
 
 
-$GroupId="GroupID";
-$NewName="NEW NAME FOR Group";
-$result=$ei->groupsupdate($GroupId, $NewName);
-echo $result["code"]=="1" ?  "Success - {$result["text"]}" : "Error #{$result["code"]}, {$result["text"]}";
-
+$GroupId = "GroupID";
+$NewName = "NEW NAME FOR Group";
+$result = $ei->groupsupdate($GroupId, $NewName);
+echo $result["code"] == "1" ? "Success - {$result["text"]}" : "Error #{$result["code"]}, {$result["text"]}";
 ?>
