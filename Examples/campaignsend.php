72d7cb73567123c779842c31702ec3fcffe13c7c
Ivelin <Ivelin@Ifko>
2016-07-12 16:23:08 +0300

    Create new API

--- Examples/campaignsend.php
+++ Examples/campaignsend.php
@@ -8,16 +8,16 @@
  * permission of emailinvest-app1.
  */
 
-include ("LatestApi/Emailinvest.php");
+include ("../LatestApi/Emailinvest.php");
 
-use Api;
+use api;
 
-$ei = new Api\Emailinvest("da39a3ee5e6b4b0d3255bfef95601890afd80709", "ifkooo", true);
+$ei = new api\Emailinvest("da39a3ee5e6b4b0d3255bfef95601890afd80709", "ifkooo", true);
 $ei->screen = "array"; // - option for php array
 $ei->host = "app1.emailinvest.com/api";
 
 /**
- * Campaign structore
+ * Campaign structure 
  * 
  * You can use remote html with file_get_contents("https://www.google.com/");
  * 
@@ -25,26 +25,41 @@ $ei->host = "app1.emailinvest.com/api";
  * 
  * @var array $data
  */
-$data["from_id"] = 1753;
+$data["from_id"] = 1753; // Get ID from SETTINGS -> SETTING FOR SEND
 $data["subject"] = "Нова Тестова кампания от API";
 $data["message"] = "Примерен <h1>HTML</h1> текст"; // 
-$data["date"] = "02.02.2016";
+$data["date"] = FALSE;
 
 /**
  * Recipients and filters
  * 
+ * All groups put in array
+ *
+ * Get groups id from menu CONTACTS -> LISTS
+ * 
+ * 
  * For filter condition, you can use 'any' or 'all' 
+ *  
+ * All filter's id put in array
+ * 
+ * Get groups id from menu CONTACTS -> FILTERS
+ * 
  * 
  * @var array $data
  */
-$recipients["groups"][] = 9369;
+$recipients["groups"][] = 10854;
 $recipients["filters"]["use"] = "any";
 $recipients["filters"]["list"][] = 77;
 
 /**
  * Settings for campaign
  * 
- * All setigns are false by default.
+ * All settings are FALSE by default.
+ * 
+ * track - Set True to track your opens and clicks
+ * ga - Set for Google Analitycs campaign
+ * inform - begin - Information message when campaign start
+ * inform - end - Information message when campaign end
  * 
  * @var array $data
  */
