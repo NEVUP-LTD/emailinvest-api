4f9e0f59b8730264e2a5b6aa8dcdc788321f2ab8
Ivelin <Ivelin@Ifko>
2016-09-27 03:19:57 +0300

    Add Emailinvest result to DB, for statistics

--- Examples/stats.php
+++ Examples/stats.php
@@ -10,7 +10,7 @@ include '../LatestApi/Emailinvest.php';
 
 use api;
 
-$ei = new api\emailinvest("da39a3ee5e6b4b0d3255bfef95601890afd80709 ", "ifkooo", true);
+$ei = new api\emailinvest("your_api_key", "your_username", true);
 $ei->Host = "app1.emailinvest.com/api";
 
 $result = $ei->getStats(49633);
