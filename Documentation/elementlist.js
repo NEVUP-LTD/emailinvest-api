0d279611b1fc4a6d311c28f9da95e86bb6f084aa
Ifkooo <ifkooo@gmail.com>
2016-11-10 18:11:18 +0200

    Remove old docs

--- Documentation/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Api\\ApiCampaigns"],["c","Api\\ApiContacts"],["c","Api\\ApiFieldsGroups"],["c","Api\\ApiSimples"],["c","Api\\ApiUnused"],["c","Api\\Emailinvest"],["c","emailinvest"]];
